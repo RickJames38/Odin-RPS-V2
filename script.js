e59b7911965888e00a74afca0b3c77489b0626b1
Nathan Moore <Wildcats25jn@gmail.com>
2024-07-26 17:14:35 -0400

    updates v2

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
 
+function resetGame() {
+    //reset game
+}
+
 function playGame() {
-    for (let i = 1; i <= 5; i++){
-        playRound(i);
-    }
-    winCounter();
-    //play the game
-    //play 5 rounds
-    //console based
-}
-
-function playRound(round) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    //console.log(computerSelection);
-    const winner = checkWinner(humanSelection, computerSelection);
-    winners.push(winner);
-    logRound(humanSelection, computerSelection, winner, round);
-    //console.log(winner);
-    //run both user input and generate computer choice
-    //check winner of each round 
-}
-
-function getHumanChoice() {
-    let userInput = prompt("Rock, Paper, Scissors?");
-    while (userInput == null) {
-        userInput = prompt("Rock, Paper, Scissors?");
-    }
-    userInput = userInput.toLowerCase();
-    let check = checkInput(userInput);
-    while (check == false) {
-        userInput = prompt("Check your spelling of your input.");
-        while (userInput == null) {
-            input = prompt("Rock, Paper, Scissors?");
+    // play the game until someone wins five times
+    let imgs = document.querySelectorAll('img')
+    imgs.forEach((img) =>
+    img.addEventListener(('click'), () => {
+        if (img.id) {
         }
-        userInput = userInput.toLowerCase();
-        check = checkInput(userInput);
+      })
+    );
+}
+
+function playRound(playerChoice) {
+    let wins = checkWins();
+    if(wins >= 5){
+        return
     }
-    return userInput;
-    //console.log(userInput);
-    //get input from player
-    //make case insenstive using .toLowerCase
+
+    const computerSelection = getComputerChoice();
+
+    const winner = checkWinner(humanSelection, computerSelection);
+
+    winners.push(winner);
+    tallyWins();
+    displayRound(playerChoice, computerChoice, winner);
+    wins = checkWins();
+    if (wins == 5){
+        //display end results
+        //change the button to visible
+        //change the text to display winner
+        displayEnd()
+    }
+}
+
+function displayEnd() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+
+    if (playerWins == 5) {
+        document.querySelector(".winner").textContent = "You Won 5 Games, Congrats!";
+    } else {
+        document.querySelector(".winner").textContent = "Sorry, The Computer Won 5 Times";
+    }
+    document.querySelector(".reset").computedStyleMap.display = "flex";
+}
+
+function displayRound(playerChoice, computerChoice, winner) {
+    document.querySelector('.playerChoice').textContent = `You Chose: ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}`;
+    document.querySelector('.computerChoice').textContent = `You Chose: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
+    document.querySelector('.ties').textContent = `Ties: ${ties}`;
+}
+
+function tallyWins() {
+    let playerWins = winners.filter((item) => item == "You Win!").length;
+    let computerWins = winners.filter((item) => item == "Computer Wins!").length;
+    let ties = winners.filter((item) => item == "It's a tie!").length;
+    document.querySelector('.playerScore').textContent = `Score${playerWins}`;
+    document.querySelector('.computerScore').textContent = `Score${computerWins}`;
+    document.querySelector('.ties').textContent = `Score${ties}`;
 }
 
 function getComputerChoice() {
@@ -50,9 +70,10 @@ function getComputerChoice() {
     //use Math.Random to generate random string
 }
 
-function checkInput(choice) {
-    return choices.includes(choice);
-    //check to see if user put in valid choice
+function checkWins() {
+    let playerWins = winners.filter((item) => item == "You Win!").length;
+    let computerWins = winners.filter((item) => item == "Computer Wins!").length;
+    return Math.max(playerWins,computerWins)
 }
 
 function checkWinner(humanChoice, computerChoice) {
@@ -76,21 +97,7 @@ function winCounter() {
     let playerWins = winners.filter((item) => item == "You Win!").length;
     let computerWins = winners.filter((item) => item == "Computer Wins!").length;
     let ties = winners.filter((item) => item == "It's a tie!").length;
-    console.log("Results:");
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", ties);
     //log the winner of each round and increment the score
 }
 
-function logRound(playerChoice, computerChoice, winner, round) {
-    console.log("Round:", round)
-    console.log("Player Chose:", playerChoice);
-    console.log("Computer Chose:", computerChoice);
-    console.log(winner);
-    console.log("--------------------------------");
-    //log who won each round and and who's score is higher after 5 rounds 
-}
-
-playGame();
-//call the funciton to run the game
+startGame();
